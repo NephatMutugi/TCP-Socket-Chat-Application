4671e164eaad810986dc847823163c20646917a6
Nephat Mutugi <muchirinephat5@gmail.com>
2020-02-23 16:44:24 +0300

    GridBagLayout

--- src/neph/Client.java
+++ src/neph/Client.java
@@ -19,8 +19,14 @@ public class Client extends JFrame {
         createWindow();
     }
     private void createWindow(){
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e1){
+            e1.printStackTrace();
+        }
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(900, 600);
+        setSize(880, 550);
         setLocationRelativeTo(null);
         setTitle("Client");
         mContentPane = new JPanel();
@@ -30,11 +36,18 @@ public class Client extends JFrame {
 
         //GridBagLayout
         GridBagLayout mGridBagLayout = new GridBagLayout();
-        mGridBagLayout.columnWidths = new int[]{0};
-        mGridBagLayout.rowHeights = new int[]{0};
-        mGridBagLayout.columnWeights = new double[]{Double.MIN_VALUE};
-        mGridBagLayout.rowWeights = new double[]{Double.MIN_VALUE};
+        mGridBagLayout.columnWidths = new int[]{15, 860, 5}; //Sum = 880
+        mGridBagLayout.rowHeights = new int[]{35, 475, 40}; //Sum = 550
+        mGridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+        mGridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
         mContentPane.setLayout(mGridBagLayout);
+
+        JTextArea mHistory = new JTextArea();
+        GridBagConstraints mGBCHistory = new GridBagConstraints();
+        mGBCHistory.fill = GridBagConstraints.BOTH;
+        mGBCHistory.gridx = 1;
+        mGBCHistory.gridy = 1;
+        mContentPane.add(mHistory,mGBCHistory);
 
         setVisible(true);
     }
